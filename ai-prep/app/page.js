7249beae83789918dc0a2975abba2e21a22435fc
Jyoti Yadav <balaboinjyotiyadav@gmail.com>
2025-02-16 17:28:55 +0530

    Added the basic Next.js Framework

---
+++ ai-prep/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Making AI-PREP</h2>
+      <Button>WANNA SEE?</Button>
+    </div>
+  );
+}
